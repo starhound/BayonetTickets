fc49dccf801ba4b2dc627eb5be0cc75a403f46e1
Starhound <wesleyreid0@gmail.com>
2020-12-04 08:01:46 -0500

    cleanup

--- Ticketing_Stub/Form1.cs
+++ Ticketing_Stub/Form1.cs
@@ -190,13 +190,10 @@ namespace Ticketing_Stub
 
             using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
             {
-                // Draw the screenshot into our bitmap.
                 using (Graphics g = Graphics.FromImage(bmp))
                 {
                     g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
                 }
-
-                // Do something with the Bitmap here, like save it to a file:
                 bmp.Save(imagePath, ImageFormat.Jpeg);
             }
 
